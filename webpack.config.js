7b46a2a3fbf431a45aae51b96527a6f4b9dd8cdc
MaoScut <849345372@qq.com>
2017-10-06 11:42:20 +0800

    cheap-module感觉不好定位

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'source-map',
   devServer: {
     contentBase: './dist',
   },
